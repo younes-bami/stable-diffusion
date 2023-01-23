9f8057611e0e7ceb12b7ffeea0a2b4418ad58109
Younes BAMI <younes.bami@octo.ma>
2023-01-23 09:43:25 +0100

    first

--- launch.py
+++ launch.py
@@ -148,7 +148,7 @@ def run_extension_installer(extension_dir):
         env = os.environ.copy()
         env['PYTHONPATH'] = os.path.abspath(".")
 
-        print(run(f'"{python}" "{path_installer}"', errdesc=f"Error running install.py for extension {extension_dir}", custom_env=env))
+        print(run(f'"{pythonve  }" "{path_installer}"', errdesc=f"Error running install.py for extension {extension_dir}", custom_env=env))
     except Exception as e:
         print(e, file=sys.stderr)
 
